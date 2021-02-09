34ffe536e67604767af932b175a414691c9387e0
brleinad <daniel@danielrb.dev>
2021-02-08 22:29:37 -0500

    update api url

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -4,6 +4,6 @@ axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
 axios.defaults.xsrfCookieName = 'csrftoken'
 // axios.defaults.withCredentials = true
 // axios.defaults.baseURL = 'localhost:8000/api/v1' // process.env.REACT_APP_API_URL
-axios.defaults.baseURL = 'http://localhost:8000' // process.env.REACT_APP_API_URL
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
 
 export default axios
